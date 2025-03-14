a57cf2de3aaa1fd15a6cd41e1d55eb86987b7858
RennanSMoura <rennan.moura@sptech.school>
2025-03-14 20:18:57 -0300

    Menu responsivo

--- code/site/public/js/menuresponsivo.js
+++ code/site/public/js/menuresponsivo.js
@@ -1,8 +1,4 @@
 function menuShow() {
     let menuMobile = document.querySelector('.cabecalho_mobile-menu');
-    if(menuMobile.classList.contains('open')){
-        menuMobile.classList.remove('open');
-    } else {
-        menuMobile.classList.add('open');
-    }
+    menuMobile.classList.toggle('open');  
 }
