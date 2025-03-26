719e1e6da9cda6a2684ed509e6e5341c53a1cf8e
VitorSuave <vitor.rodrigues@sptech.school>
2025-03-26 08:48:52 -0300

    ..

--- projeto-etl-xlsx/src/main/java/school/sptech/EtlProcess.java
+++ projeto-etl-xlsx/src/main/java/school/sptech/EtlProcess.java
@@ -1,25 +1,27 @@
 package school.sptech;
 
 import school.sptech.etl.extract.XlsxExtractor;
-import school.sptech.etl.transform.DataTransformer;
-import school.sptech.etl.load.DatabaseLoader;
+//import school.sptech.etl.transform.DataTransformer;
+//import school.sptech.etl.load.DatabaseLoader;
 import java.util.List;
 
 public class EtlProcess {
     public static void main(String[] args) {
         try {
             // 1. Extração
-            List<String> extractedText = XlsxExtractor.extractData("C:\\Users\\nixch\\OneDrive\\Documentos\\Github\\FlyOn\\projeto-etl-xlsx\\src\\main\\resources\\VRA_2022_01.xlsx");
+            List<String> extractedText = XlsxExtractor.extractData("C:\\Users\\vit_o\\FlyOn\\projeto-etl-xlsx\\src\\main\\resources\\VRA_2022_01.xlsx");
 
             System.out.println(extractedText);
-//            String natureza = extractedText.get(0);
-//            extractedText.remove(0);
-//
-//            // 2. Transformação
-//             List<Integer> cleanedText = DataTransformer.transformData(extractedText);
-//
-//            // 3. Carga
-//            DatabaseLoader.loadData(natureza, cleanedText);
+/*
+            String natureza = extractedText.get(0);
+            extractedText.remove(0);
+
+            // 2. Transformação
+             List<Integer> cleanedText = DataTransformer.transformData(extractedText);
+
+            // 3. Carga
+            DatabaseLoader.loadData(natureza, cleanedText);
+*/
 
         } catch (Exception e) {
             e.printStackTrace();
