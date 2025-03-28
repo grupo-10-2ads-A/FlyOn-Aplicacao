06029b93056cd60d61da5da3af94e43e57e3129e
NicolasNun <nicolassantos9283@gmail.com>
2025-03-28 13:24:19 -0300

    Extração e transformação funcionando em escala reduzida

--- projeto-etl-xlsx/src/main/java/school/sptech/EtlProcess.java
+++ projeto-etl-xlsx/src/main/java/school/sptech/EtlProcess.java
@@ -3,6 +3,8 @@ package school.sptech;
 import school.sptech.etl.extract.XlsxExtractor;
 import school.sptech.etl.transform.DataTransformer;
 import school.sptech.etl.load.DatabaseLoader;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class EtlProcess {
@@ -11,15 +13,29 @@ public class EtlProcess {
             // 1. Extração
             List<String> extractedText = XlsxExtractor.extractData("C:\\Users\\nixch\\OneDrive\\Documentos\\Github\\FlyOn\\projeto-etl-xlsx\\src\\main\\resources\\VRA_2022_01.xlsx");
 
-            System.out.println(extractedText);
-//            String natureza = extractedText.get(0);
-//            extractedText.remove(0);
-//
-//            // 2. Transformação
-//             List<Integer> cleanedText = DataTransformer.transformData(extractedText);
-//
-//            // 3. Carga
-//            DatabaseLoader.loadData(natureza, cleanedText);
+//            System.out.println(extractedText);
+
+            List<String> dateTimes = new ArrayList<>();
+            dateTimes.add(extractedText.get(0));
+            dateTimes.add(extractedText.get(1));
+            dateTimes.add(extractedText.get(2));
+            dateTimes.add(extractedText.get(3));
+
+            extractedText.remove(3);
+            extractedText.remove(2);
+            extractedText.remove(1);
+            extractedText.remove(0);
+
+
+            // 2. Transformação
+            int cleanedInt = DataTransformer.transfomDataInt(extractedText.get(extractedText.size() - 1));
+            extractedText.remove(extractedText.size() - 1);
+//            System.out.println(cleanedInt);
+            List<String> cleanedTextDateTime = DataTransformer.transformDataDateTime(dateTimes);
+//            System.out.println(cleanedTextDateTime);
+
+            // 3. Carga
+//            DatabaseLoader.loadData(cleanedTextDateTime, extractedText, cleanedInt);
 
         } catch (Exception e) {
             e.printStackTrace();
--- projeto-etl-xlsx/src/main/java/school/sptech/etl/transform/DataTransformer.java
+++ projeto-etl-xlsx/src/main/java/school/sptech/etl/transform/DataTransformer.java
@@ -5,25 +5,38 @@ import java.util.List;
 
 public class DataTransformer {
 
-    public static List<Integer> transformData(List<String> numeros) {
-        // Aqui você pode aplicar regras de transformação, se necessário.
-        // Por exemplo: converter valores, formatar "strings", ou aplicar validações.
-        List<Integer> dados = new ArrayList<>();
+    public static List<String> transformDataDateTime(List<String> dateTimes) {
+        String data_hora_partida_prevista = dateTimes.get(0);
+        String data_hora_partida_real = dateTimes.get(1);
+        String data_hora_chegada_prevista = dateTimes.get(2);
+        String data_hora_chegada_real = dateTimes.get(3);
 
-        for (String numero : numeros) {
+        String data_hora_partida_prevista_formatada =
+                data_hora_partida_prevista.substring(6, 10) + "-" + data_hora_partida_prevista.substring(3, 6) + data_hora_partida_prevista.substring(0, 2) + data_hora_partida_prevista.substring(10);
+        String data_hora_partida_real_formatada =
+                data_hora_partida_real.substring(6, 10) + "-" + data_hora_partida_real.substring(3, 6) + data_hora_partida_real.substring(0, 2) + data_hora_partida_real.substring(10);
+        String data_hora_chegada_prevista_formatada =
+                data_hora_chegada_prevista.substring(6, 10) + "-" + data_hora_chegada_prevista.substring(3, 6) + data_hora_chegada_prevista.substring(0, 2) + data_hora_chegada_prevista.substring(10);
+        String data_hora_chegada_real_formatada =
+                data_hora_chegada_real.substring(6, 10) + "-" + data_hora_chegada_real.substring(3, 6) + data_hora_chegada_real.substring(0, 2) + data_hora_chegada_real.substring(10);
 
-            try {
-                if(numero.contains(".")) {
-                    numero = numero.replace(".", "");
-                }
-                dados.add(Integer.parseInt(numero));
-            } catch (NumberFormatException e) {
-                System.err.println("Erro ao converter '" + numero + "' para inteiro. Ignorando esse valor.");
-            }
-        }
+        data_hora_partida_prevista_formatada = data_hora_partida_prevista_formatada.replaceAll("/", "-");
+        data_hora_partida_real_formatada = data_hora_partida_real_formatada.replaceAll("/", "-");
+        data_hora_chegada_prevista_formatada = data_hora_chegada_prevista_formatada.replaceAll("/", "-");
+        data_hora_chegada_real_formatada = data_hora_chegada_real_formatada.replaceAll("/", "-");
 
-        System.out.println("Passei no DataTransformer");
-        System.out.println(dados);
-        return dados;
+        List<String> dateTimesFormat = new ArrayList<>();
+        dateTimesFormat.add(data_hora_partida_prevista_formatada);
+        dateTimesFormat.add(data_hora_partida_real_formatada);
+        dateTimesFormat.add(data_hora_chegada_prevista_formatada);
+        dateTimesFormat.add(data_hora_chegada_real_formatada);
+
+        System.out.println("Success - Transformação da data e hora bem sucedidas");
+        return dateTimesFormat;
+    }
+
+    public static Integer transfomDataInt(String assentos_comercializados) {
+        System.out.println("Success - Transformação do número de assentos bem sucedida");
+        return Integer.parseInt(assentos_comercializados);
     }
 }
