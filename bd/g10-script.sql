c3436d5da7be4f32370454a777a2c8511eccf2c6
NicolasNun <nicolassantos9283@gmail.com>
2025-04-13 15:11:40 -0300

    Criação da tabela sugestao

--- bd/g10-script.sql
+++ bd/g10-script.sql
@@ -7,20 +7,20 @@ create table agencia (
     cnpj char(14) unique not null,
     nome_fantasia varchar(100) unique not null,
     razao_social varchar(100) unique not null,
-    representante_legal varchar(100) not null,
-    senha varchar(20) not null
+    representante_legal varchar(100) not null
 );
 
 -- Tabela de Endereço com relação 1:1 com Agência
 create table endereco (
-    id int primary key auto_increment,
-    fk_agencia int unique,
+    id int auto_increment,
+    fk_agencia int,
     cep char(8) not null,
     numero int not null,
     logradouro varchar(100) not null,
     bairro varchar(100) not null,
     cidade varchar(100) not null,
     estado char(2) not null,
+    constraint pk_endereco_agencia primary key(id, fk_agencia),
     constraint fk_endereco_agencia foreign key (fk_agencia) references agencia(idAgencia) on delete cascade
 );
 
@@ -33,6 +33,29 @@ create table usuario (
     email varchar(100) unique not null,
     senha varchar(20) not null,
     constraint fk_usuario_agencia foreign key (fk_agencia) references agencia(idAgencia) on delete cascade
+);
+
+create table prefil_cliente (
+	id int auto_increment,
+    fk_agencia int,
+	perfil varchar(45) not null,
+	mais_barato int default(0),
+	menos_cancelamentos int default(0),
+	menos_atrasos int default(0),
+    constraint pk_perfil_agencia primary key(id, fk_agencia),
+    constraint fk_perfil_agencia foreign key (fk_agencia) references agencia(idAgencia) on delete cascade,
+	constraint chk_mais_barato check (mais_barato in (0, 1)),
+	constraint chk_menos_cancelamentos check (menos_cancelamentos in (0, 1)),
+	constraint chk_menos_atrasos check (menos_atrasos in (0, 1))
+);
+
+create table notificações (
+	id int auto_increment,
+    fk_agencia int,
+    data_hora datetime not null,
+    mensagem varchar(255) not null,
+    constraint pk_notificacao_agencia primary key(id, fk_agencia),
+    constraint fk_notificacao_agencia foreign key (fk_agencia) references agencia(idAgencia) on delete cascade
 );
 
 create table historico_passagens (
@@ -51,29 +74,22 @@ create table historico_passagens (
 	assentos_comercializados int
 );
 
-create table prefil_cliente (
-	id int primary key auto_increment,
-	perfil varchar(45),
-	mais_barato int,
-	menos_cancelamentos int,
-	menos_atrasos int,
-	constraint chk_mais_barato check (mais_barato in (0, 1)),
-	constraint chk_menos_cancelamentos check (menos_cancelamentos in (0, 1)),
-	constraint chk_menos_atrasos check (menos_atrasos in (0, 1))
+create table sugestao (
+	id int auto_increment,
+    fk_agencia int,
+    fk_passagem int,
+    descricao varchar(100),
+    constraint pk_sugestao_passagem_agencia primary key(id, fk_agencia, fk_passagem),
+    constraint fk_sugestao_agencia foreign key (fk_agencia) references agencia(idAgencia) on delete cascade,
+    constraint fk_sugestao_passagem foreign key (fk_passagem) references historico_passagens(id) on delete cascade
 );
 
 create table registro (
 	id int primary key auto_increment,
 	data_hora datetime not null,
-	classificacao char(7),
-	mensagem varchar(255),
+	classificacao char(7) not null,
+	mensagem varchar(255) not null,
 	constraint chk_classificacao check (classificacao in ('TRACE', 'WARN', 'INFO', 'SUCCESS'))
-);
-
-create table notificacoes (
-	id int primary key auto_increment,
-    	data_hora datetime,
-    	mensagem varchar(255)
 );
 
 -- select * from agencia;
