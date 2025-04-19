0d254b7e69079bf5a270f0730ad6a40fe8dd515c
VitorSuave <vitor.rodrigues@sptech.school>
2025-04-19 14:30:26 -0300

    feat: atualização do ETL para suporte ao teste localmente e em nuvem

--- projeto-etl-xlsx/src/main/java/school/sptech/etl/extract/XlsxExtractor.java
+++ projeto-etl-xlsx/src/main/java/school/sptech/etl/extract/XlsxExtractor.java
@@ -17,147 +17,147 @@ public class XlsxExtractor {
     private static boolean usandoStream = false;
 
     // Carregando a planilha direto do S3
-    public static void loadWorkbook(InputStream inputStream) throws IOException {
-        closeWorkbook();
-        cachedWorkbook = new XSSFWorkbook(inputStream);
-        usandoStream = true;
-    }
-
-    // Versão para testar localmente
-//    private static void initWorkbookCache(String filePath) throws IOException {
-//        if (!filePath.equals(cachedFilePath)) {
-//            closeWorkbook();
-//            cachedWorkbook = new XSSFWorkbook(new FileInputStream(new File(filePath)));
-//            cachedFilePath = filePath;
-//            usandoStream = false;
-//        }
+//    public static void loadWorkbook(InputStream inputStream) throws IOException {
+//        closeWorkbook();
+//        cachedWorkbook = new XSSFWorkbook(inputStream);
+//        usandoStream = true;
 //    }
 
-    // Versão para testar na EC2
-    public static Integer returnTotalRows() {
-        if (cachedWorkbook == null) {
-            throw new IllegalStateException("O workbook não foi carregado.");
+    // Versão para testar localmente
+    private static void initWorkbookCache(String filePath) throws IOException {
+        if (!filePath.equals(cachedFilePath)) {
+            closeWorkbook();
+            cachedWorkbook = new XSSFWorkbook(new FileInputStream(new File(filePath)));
+            cachedFilePath = filePath;
+            usandoStream = false;
         }
-        Sheet sheet = cachedWorkbook.getSheetAt(0);
-        return sheet.getLastRowNum();
     }
 
-    // Versão para testar localmente
-//    public static Integer returnTotalRows(String filePath) throws IOException {
-//        if (!usandoStream) initWorkbookCache(filePath);
+    // Versão para testar na EC2
+//    public static Integer returnTotalRows() {
+//        if (cachedWorkbook == null) {
+//            throw new IllegalStateException("O workbook não foi carregado.");
+//        }
 //        Sheet sheet = cachedWorkbook.getSheetAt(0);
 //        return sheet.getLastRowNum();
 //    }
 
     // Versão para testar localmente
-//    public static List<String> extractData(String filePath, int currentRow) throws IOException {
-//        if (!usandoStream) initWorkbookCache(filePath);
-//        Sheet sheet = cachedWorkbook.getSheetAt(0);
-//        Row row = sheet.getRow(currentRow);
-//        List<String> res = new ArrayList<>();
-//        if (row != null) {
-//            row.getCell(6).setCellType(CellType.STRING);
-//
-//            String data_hora_partida_prevista = formatarDataHoraCelula(row.getCell(9));
-//            String data_hora_partida_real = formatarDataHoraCelula(row.getCell(10));
-//            String data_hora_chegada_prevista = formatarDataHoraCelula(row.getCell(13));
-//            String data_hora_chegada_real = formatarDataHoraCelula(row.getCell(14));
-//            String sigla_empresa_aerea = formatarSeNulo(row.getCell(0));
-//            String empresa_aerea = formatarSeNulo(row.getCell(1));
-//            String origem = formatarSeNulo(row.getCell(8));
-//            String destino = formatarSeNulo(row.getCell(12));
-//            String situacao_voo = formatarSeNulo(row.getCell(15));
-//            String situacao_partida = formatarSeNulo(row.getCell(18));
-//            String situacao_chegada = formatarSeNulo(row.getCell(19));
-//            String assentos_comercializados = formatarSeNulo(row.getCell(6));
-//
-//            Collections.addAll(res,
-//                    data_hora_partida_prevista,
-//                    data_hora_partida_real,
-//                    data_hora_chegada_prevista,
-//                    data_hora_chegada_real,
-//                    sigla_empresa_aerea,
-//                    empresa_aerea,
-//                    origem,
-//                    destino,
-//                    situacao_voo,
-//                    situacao_partida,
-//                    situacao_chegada,
-//                    assentos_comercializados
-//            );
-//        }
-//        return res;
-//    }
-
-    // Versão para usar na EC2
-    public static List<List<String>> extractBatchData(int startRow, int batchSize) {
-        if (cachedWorkbook == null) {
-            throw new IllegalStateException("O workbook não foi carregado.");
-        }
-
+    public static Integer returnTotalRows(String filePath) throws IOException {
+        if (!usandoStream) initWorkbookCache(filePath);
         Sheet sheet = cachedWorkbook.getSheetAt(0);
-        List<List<String>> batchData = new ArrayList<>(batchSize);
-        int endRow = Math.min(startRow + batchSize - 1, sheet.getLastRowNum());
-
-        for (int rowNum = startRow; rowNum <= endRow; rowNum++) {
-            Row row = sheet.getRow(rowNum);
-            List<String> res = new ArrayList<>();
-
-            if (row != null) {
-                Cell cell6 = row.getCell(6);
-                if (cell6 != null) {
-                    cell6.setCellType(CellType.STRING);
-                }
-
-                String data_hora_partida_prevista = formatarDataHoraCelula(row.getCell(9));
-                String data_hora_partida_real = formatarDataHoraCelula(row.getCell(10));
-                String data_hora_chegada_prevista = formatarDataHoraCelula(row.getCell(13));
-                String data_hora_chegada_real = formatarDataHoraCelula(row.getCell(14));
-                String sigla_empresa_aerea = formatarSeNulo(row.getCell(0));
-                String empresa_aerea = formatarSeNulo(row.getCell(1));
-                String origem = formatarSeNulo(row.getCell(8));
-                String destino = formatarSeNulo(row.getCell(12));
-                String situacao_voo = formatarSeNulo(row.getCell(15));
-                String situacao_partida = formatarSeNulo(row.getCell(18));
-                String situacao_chegada = formatarSeNulo(row.getCell(19));
-                String assentos_comercializados = formatarSeNulo(row.getCell(6));
-
-                Collections.addAll(res,
-                        data_hora_partida_prevista,
-                        data_hora_partida_real,
-                        data_hora_chegada_prevista,
-                        data_hora_chegada_real,
-                        sigla_empresa_aerea,
-                        empresa_aerea,
-                        origem,
-                        destino,
-                        situacao_voo,
-                        situacao_partida,
-                        situacao_chegada,
-                        assentos_comercializados
-                );
-            }
-
-            batchData.add(res);
-        }
-
-        return batchData;
+        return sheet.getLastRowNum();
     }
 
-    // Versão para testar localmente
-//    public static List<List<String>> extractBatchData(String filePath, int startRow, int batchSize) throws IOException {
-//        if (!usandoStream) initWorkbookCache(filePath);
+//     Versão para testar localmente
+    public static List<String> extractData(String filePath, int currentRow) throws IOException {
+        if (!usandoStream) initWorkbookCache(filePath);
+        Sheet sheet = cachedWorkbook.getSheetAt(0);
+        Row row = sheet.getRow(currentRow);
+        List<String> res = new ArrayList<>();
+        if (row != null) {
+            row.getCell(6).setCellType(CellType.STRING);
+
+            String data_hora_partida_prevista = formatarDataHoraCelula(row.getCell(9));
+            String data_hora_partida_real = formatarDataHoraCelula(row.getCell(10));
+            String data_hora_chegada_prevista = formatarDataHoraCelula(row.getCell(13));
+            String data_hora_chegada_real = formatarDataHoraCelula(row.getCell(14));
+            String sigla_empresa_aerea = formatarSeNulo(row.getCell(0));
+            String empresa_aerea = formatarSeNulo(row.getCell(1));
+            String origem = formatarSeNulo(row.getCell(8));
+            String destino = formatarSeNulo(row.getCell(12));
+            String situacao_voo = formatarSeNulo(row.getCell(15));
+            String situacao_partida = formatarSeNulo(row.getCell(18));
+            String situacao_chegada = formatarSeNulo(row.getCell(19));
+            String assentos_comercializados = formatarSeNulo(row.getCell(6));
+
+            Collections.addAll(res,
+                    data_hora_partida_prevista,
+                    data_hora_partida_real,
+                    data_hora_chegada_prevista,
+                    data_hora_chegada_real,
+                    sigla_empresa_aerea,
+                    empresa_aerea,
+                    origem,
+                    destino,
+                    situacao_voo,
+                    situacao_partida,
+                    situacao_chegada,
+                    assentos_comercializados
+            );
+        }
+        return res;
+    }
+
+    // Versão para usar na EC2
+//    public static List<List<String>> extractBatchData(int startRow, int batchSize) {
+//        if (cachedWorkbook == null) {
+//            throw new IllegalStateException("O workbook não foi carregado.");
+//        }
+//
 //        Sheet sheet = cachedWorkbook.getSheetAt(0);
 //        List<List<String>> batchData = new ArrayList<>(batchSize);
-//
 //        int endRow = Math.min(startRow + batchSize - 1, sheet.getLastRowNum());
 //
 //        for (int rowNum = startRow; rowNum <= endRow; rowNum++) {
-//            batchData.add(extractData(filePath, rowNum));
+//            Row row = sheet.getRow(rowNum);
+//            List<String> res = new ArrayList<>();
+//
+//            if (row != null) {
+//                Cell cell6 = row.getCell(6);
+//                if (cell6 != null) {
+//                    cell6.setCellType(CellType.STRING);
+//                }
+//
+//                String data_hora_partida_prevista = formatarDataHoraCelula(row.getCell(9));
+//                String data_hora_partida_real = formatarDataHoraCelula(row.getCell(10));
+//                String data_hora_chegada_prevista = formatarDataHoraCelula(row.getCell(13));
+//                String data_hora_chegada_real = formatarDataHoraCelula(row.getCell(14));
+//                String sigla_empresa_aerea = formatarSeNulo(row.getCell(0));
+//                String empresa_aerea = formatarSeNulo(row.getCell(1));
+//                String origem = formatarSeNulo(row.getCell(8));
+//                String destino = formatarSeNulo(row.getCell(12));
+//                String situacao_voo = formatarSeNulo(row.getCell(15));
+//                String situacao_partida = formatarSeNulo(row.getCell(18));
+//                String situacao_chegada = formatarSeNulo(row.getCell(19));
+//                String assentos_comercializados = formatarSeNulo(row.getCell(6));
+//
+//                Collections.addAll(res,
+//                        data_hora_partida_prevista,
+//                        data_hora_partida_real,
+//                        data_hora_chegada_prevista,
+//                        data_hora_chegada_real,
+//                        sigla_empresa_aerea,
+//                        empresa_aerea,
+//                        origem,
+//                        destino,
+//                        situacao_voo,
+//                        situacao_partida,
+//                        situacao_chegada,
+//                        assentos_comercializados
+//                );
+//            }
+//
+//            batchData.add(res);
 //        }
 //
 //        return batchData;
 //    }
+
+    // Versão para testar localmente
+    public static List<List<String>> extractBatchData(String filePath, int startRow, int batchSize) throws IOException {
+        if (!usandoStream) initWorkbookCache(filePath);
+        Sheet sheet = cachedWorkbook.getSheetAt(0);
+        List<List<String>> batchData = new ArrayList<>(batchSize);
+
+        int endRow = Math.min(startRow + batchSize - 1, sheet.getLastRowNum());
+
+        for (int rowNum = startRow; rowNum <= endRow; rowNum++) {
+            batchData.add(extractData(filePath, rowNum));
+        }
+
+        return batchData;
+    }
 
     public static void closeWorkbook() throws IOException {
         if (cachedWorkbook != null) {
