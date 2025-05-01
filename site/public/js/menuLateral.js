24846f3cae6ebaa65846d0aa3d251ff5c964e3a6
Diaseduarda01 <maria.dias@sptech.school>
2025-05-01 19:54:39 -0300

    fix(app): Corrigido caminhos

--- site/public/js/menuLateral.js
+++ site/public/js/menuLateral.js
@@ -1,4 +1,10 @@
 function mostrarItenMenu() {
     const header = document.querySelector("header");
     header.classList.toggle("expandido");
+
+    if (header.classList.contains("expandido")) {
+      iconeMenu.src = "./img/excluir.png"; 
+     } else {
+      iconeMenu.src = "./img/icons8-hamburguer.svg"; 
+    }
   }
