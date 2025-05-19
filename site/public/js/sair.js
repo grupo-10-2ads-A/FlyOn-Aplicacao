336d762faa83b6f314c96392acbc6ae9e76f5bd6
Diaseduarda01 <maria.dias@sptech.school>
2025-05-19 08:47:27 -0300

    refactor: alterando repositorio para somente automação

--- site/public/js/sair.js
+++
@@ -1,7 +0,0 @@
-function logout() {
-  
-    localStorage.removeItem('ID_USUARIO');
-    localStorage.removeItem('NOME_USUARIO');
-    localStorage.removeItem('EMAIL_USUARIO');
-    window.location.href = '../auth/login.html';
-}
--- site/src/routes/agencia.js
+++
@@ -1,7 +0,0 @@
-var express = require("express");
-var router = express.Router();
-var agenciaController = require("../controllers/agenciaController");
-
-router.post("/cadastrar", agenciaController.cadastrarAgencia);
-
-module.exports = router;
